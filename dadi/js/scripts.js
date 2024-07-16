d6377cd6dc1673a7a0f6c99c28c3b8e2a1341c04
AleScla <scla.alessandro@gmail.com>
2024-07-16 15:59:24 +0200

    inserita parte html inserito un po' di stile nel css strutturato codice js per farlo funzionare PS: da modificare per attivarlo solo quando si clicca il button

--- dadi/js/scripts.js
+++ dadi/js/scripts.js
@@ -1,1 +1,22 @@
-console.log('js funziona')
+console.log('js funziona');
+
+const randomNMB = Math.floor((Math.random() * 6) + 1);
+console.log(randomNMB);
+
+const randomNMB2 = Math.floor((Math.random() * 6) + 1);
+console.log(randomNMB2);
+
+document.getElementById('pc').innerHTML = randomNMB;
+document.getElementById('utente').innerHTML = randomNMB2;
+
+if (randomNMB < randomNMB2){
+    console.log('pc vince');
+    
+}
+
+else if (randomNMB == randomNMB2){
+    console.log('pareggio');
+}
+else{
+    console.log('user vince');
+}
